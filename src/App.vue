8005ac25bb63a4b890aa6bc3b0ac277f447dd3a5
yaoandy107 <yaoandy107@gmail.com>
2019-10-26 19:50:41 +0800

    Reset global style

--- src/App.vue
+++ src/App.vue
@@ -16,24 +16,11 @@ export default {
 </script>
 
 <style lang="scss">
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+@import url('https://fonts.googleapis.com/css?family=Noto+Sans+TC&display=swap');
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+* {
+  font-size: 15px;
+  font-family: 'Noto Sans TC', sans-serif;
+  color: #435058;
 }
 </style>
