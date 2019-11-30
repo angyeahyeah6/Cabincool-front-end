bf975fb187a38e15c36d1c63656da2ccdc126465
Adidan Tsai <bill3129066@gmail.com>
2019-11-30 14:13:17 +0800

    half home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -31,13 +31,42 @@
         </el-col>
         <el-col :span="11">
           <div class="home-container">
-            <h1 class="title-18" >我要發問</h1>
-              <input type="text" class="input-4" placeholder="  問題內容">
-                <router-link class="router-link" :to="{ name: 'home'}">
-                <button class="submit-button-1" style="margin-top:60px;height:37px">送出</button>
-                </router-link>
-            </div>
+            <h1 class="title-18" >熱門問題</h1>
+          </div>
+          <el-row>
+            <el-col :span="4">
+              <h1>社會</h1>
+              <h1>商業</h1>
+              <h1>健康</h1>
+              <h1>科技</h1>
+            </el-col>
+            <el-col :span="4">
+              <h1>科學</h1>
+              <h1>設計</h1>
+              <h1>其他</h1>
+            </el-col>
+          </el-row>
+          <img src="../assets/截圖 2019-11-29 04.03.28.png" class="home-img">
         </el-col>
+    </el-row>
+  </div>
+  <div>
+    <el-row class="home-section">
+      <el-col :span="8">
+        <img src="../assets/fun1.png" width="50%">
+        <h1 class="title-18" >小小問題大大解答</h1>
+        <h1 class="title-18" >在 Cabincool 上我們透過獎金制度吸引現實世界中真正的專家，並透過評分制度選出最佳的答案，讓您每一個問題都有著最高品質的答案。</h1>
+      </el-col>
+      <el-col :span="8">
+        <img src="../assets/fun2.png" width="50%">
+        <h1 class="title-18" >市場機制下的評選機制</h1>
+        <h1 class="title-18" >提問者可以設定要怎麼分配獎金與如何選擇答案，不同於其他平台，Cabincool 可以選擇開放一般使用者也加入評選，透過眾人的智慧選出最好的答案！</h1>
+      </el-col>
+      <el-col :span="8">
+        <img src="../assets/fun3.png" width="50%">
+        <h1 class="title-18" >回答問題以賺取獎金</h1>
+        <h1 class="title-18" >您可以在 Cabincool 上尋找有趣的問題，並在任一您習慣的平台（如 Medium）上發表回覆的文章，就有機會可以讓您的回答被眾人看見並且賺取獎金。</h1>
+      </el-col>
     </el-row>
   </div>
 </div>
